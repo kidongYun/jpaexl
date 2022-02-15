abc9b6fff5a0da95ae1335fbc34cfb120c0288e8
kian.yun <kian.yun@kakaobank.com>
2022-02-15 22:20:08 +0900

    add generic type into tuple

--- src/main/java/com/kian/yun/jpaexl/domain/Tuple.java
+++ src/main/java/com/kian/yun/jpaexl/domain/Tuple.java
@@ -8,19 +8,21 @@ import java.util.List;
 
 @Getter
 @Builder
-public class Tuple {
+public class Tuple<T> {
+    private final Class<T> clazz;
     private final List<Data<?>> value;
 
-    private Tuple(List<Data<?>> tuple) {
+    private Tuple(Class<T> clazz, List<Data<?>> tuple) {
+        this.clazz = clazz;
         this.value = tuple;
     }
 
-    public static Tuple empty() {
-        return new Tuple(new ArrayList<>());
+    public static <T> Tuple<T> empty() {
+        return new Tuple<>(null, new ArrayList<>());
     }
 
-    public static Tuple of(List<Data<?>> tuple) {
-        return new Tuple(tuple);
+    public static <T> Tuple<T> of(Class<T> clazz, List<Data<?>> tuple) {
+        return new Tuple<>(clazz, tuple);
     }
 
     public void add(Data<?> data) {
